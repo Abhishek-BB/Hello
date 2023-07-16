4b0218fe7f9c43915c8e2984eb84ada39feea3aa
Abhishek-BB <139685312+Abhishek-BB@users.noreply.github.com>
2023-07-16 16:52:13 +0530

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,69 @@
+
+#data types
+
+# 1 int
+''''
+num1 = 100
+print(num1)
+print(id(num1))
+'''
+'''''
+a = 3 
+b = 8
+
+print(id(a))
+print(a)
+print(id(b))
+print(b)
+'''
+
+# string
+'''''
+s = "'python' is a languange"
+print(s)
+print(id(s))                                                                                                                                                                                                           
+'''''
+#list data types :[]  append is function of list
+'''''
+l = [3,4,2,6,6,7,8,2,]
+print(l)
+'''''
+'''''
+l2 = [3,5,8,"hey","yo"]
+print(id(l2))
+print(l2)
+l2.append(100)
+'''''
+
+#tuple
+'''''
+t = (34,56,86)
+
+print(t)
+'''
+# dictionary
+'''''
+d = {"name" : "Edyoda" , "email" : "abc@edyoda"}
+print(d)
+'''''
+
+#set
+'''''
+s = {28,56,78,38}
+print(s)
+'''
+
+#Bool : True False
+'''''
+# complex
+a = 2 + 8j
+b = 5 + 3j
+c = a + b
+
+print(c)
+'''
+
+num = input("Enter a no.")
+
+print("No. is", num)
+
